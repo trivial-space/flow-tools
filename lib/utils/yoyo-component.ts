76e3b118b5a82b2fedfe086118990a1e9336b949
trival <thomas.gorny@mail.com>
2017-03-14 12:03:41 +0100

    update yoyo component to update root element styles and only update on raf

--- lib/utils/yoyo-component.ts
+++ lib/utils/yoyo-component.ts
@@ -25,7 +25,32 @@ export interface Component {
 }
 
 
-export function flowComponentFactory(stateFlow: Runtime, dispatchId: string): Component {
+let rafActions = {}
+let callRaf = true
+
+function executeRafActions () {
+  for (let key in rafActions) {
+    rafActions[key]()
+  }
+  rafActions = {}
+  callRaf = true
+}
+
+function updateOnAnimationFrame(key: string, fn: Function) {
+  if (callRaf) {
+    requestAnimationFrame(executeRafActions)
+    callRaf = false
+  }
+  rafActions[key] = fn
+}
+
+
+let componentCount = 0
+
+export function flowComponentFactory(
+  stateFlow: Runtime,
+  dispatchId: string
+): Component {
 
   function dispatch(action: Action) {
     stateFlow.set(dispatchId, action)
@@ -35,21 +60,39 @@ export function flowComponentFactory(stateFlow: Runtime, dispatchId: string): Co
     const firstState = stateFlow.get(viewStateId)
 
     const element = template(firstState, dispatch, component)
+    const styleKeys = Object.keys(element.style)
+
+    const cid = 'c' + componentCount++
+
     element.dataset.tvsComponent = "component"
 
-    const update = newState => {
+    const updateElement = () => {
+      const newState =  stateFlow.get(viewStateId)
+
       const newElement = template(newState, dispatch, component)
+
       console.log('updating', element)
+
       yo.update(element, newElement, {
         getNodeKey: function(node) {
           return node.id || (node.dataset && node.dataset.key);
         },
         childrenOnly: true,
         onBeforeElUpdated: function(fromEl: HTMLElement) {
-          return fromEl.dataset.tvsComponent !== "component";
-        },
+          return fromEl.dataset.tvsComponent !== "component"
+        }
       })
+
+      for (let i = 0; i < styleKeys.length; i++) {
+        let key = styleKeys[i];
+        if (element.style[key] !== newElement.style[key]) {
+          console.log("updating style property", key)
+          element.style[key] = newElement.style[key]
+        }
+      }
     }
+
+    const update = () => updateOnAnimationFrame(cid, updateElement)
 
     const onload = () => {
       console.log('element inserted into dom!', element)
